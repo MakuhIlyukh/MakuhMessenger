07c87a1febb144f2e56c3c128e035c1042c3fa59
Makukha_Ilia <makukha.it@gmail.com>
2021-05-09 00:52:20 +0300

    First commit

---
+++ client.py
@@ -0,0 +1,167 @@
+import tkinter as tk
+import socket
+from threading import Thread, Event
+from select import select
+from warnings import warn
+
+#server address
+HOST = '127.0.0.1'
+PORT = 28344
+
+def disable(elem):
+    elem.config(state=tk.DISABLED)
+
+def enable(elem):
+    elem.config(state=tk.NORMAL)
+
+class Chat_window:
+    def __handle_readable(self):
+        if self.com_con in self.readable:
+            self.server.send(self.com_con.recv(4096))
+        if self.server in self.readable:
+            data = self.server.recv(4096)
+            if data:
+                self.add_message(data.decode('utf-8'))
+            else:
+                self.server.close()
+                self.inputs.remove(self.server)
+                self.exit_event.set()
+                self.com_sender.send(b'q')
+
+
+    def __handle_writable(self):
+        warn('реализуй')
+
+    def __handle_exceptional(self):
+        warn('реализуй')
+
+    def __create_com_listener(self):
+        com_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        com_listener.bind((COM_LISTENER_HOST, COM_LISTENER_PORT))
+        com_listener.setblocking(False)
+        com_listener.listen()
+        return com_listener
+    
+    def __create_com_sender(self):
+        com_sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        com_sender.connect((COM_LISTENER_HOST, COM_LISTENER_PORT))
+        com_sender.setblocking(False)
+        return com_sender
+
+    def __tarfun(self):
+        self.com_listener = self.__create_com_listener()
+        self.com_sender = self.__create_com_sender()
+        self.com_con, _ = self.com_listener.accept()
+        self.com_con.setblocking(False)
+
+        self.inputs = [self.server, self.com_con]
+        self.outputs = []
+
+        self.exit_event = Event()
+        
+        while True:
+            self.readable, self.writable, self.exceptional = select(self.inputs, self.outputs, self.inputs)
+            if self.exit_event.is_set():
+                break
+            self.__handle_readable()
+            self.__handle_writable()
+            self.__handle_exceptional()
+
+        for elem in [self.server, self.com_listener, self.com_sender, self.com_con]:
+            elem.close()
+
+    def __init__(self, username):
+        self.username = username
+
+        self.window = tk.Tk()
+
+        self.f_top = tk.Frame(self.window)
+        self.f_bot = tk.Frame(self.window)
+        self.f_top.pack()
+        self.f_bot.pack()
+
+        #Text
+        self.txtbox = tk.Text(self.f_top, width=50, height=10)
+        self.txtbox.pack(side=tk.LEFT)
+        disable(self.txtbox)
+
+        #scroll
+        self.scroll = tk.Scrollbar(self.f_top, command=self.txtbox.yview)
+        self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
+        self.txtbox.config(yscrollcommand=self.scroll.set)
+        
+        # Entry
+        self.entry = tk.Entry(self.f_bot, width=50)
+        self.entry.pack(side=tk.LEFT)
+
+        #send Button
+        self.send_btn = tk.Button(self.f_bot, text='Send', width=10, height=1, command=self.__send_btn_click)        
+        self.send_btn.pack(side=tk.LEFT)    
+
+        # connect to server
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.connect((HOST, PORT))
+        self.server.setblocking(False)
+
+        # start connection thread
+        con_thread = Thread(target=self.__tarfun)
+        con_thread.start()
+
+
+        self.window.mainloop()
+
+    def __send_btn_click(self):
+        message = self.entry.get()
+        self.server.send((self.username + ' : ' + message).encode('utf-8'))
+        self.add_message(self.username + ' : ' + message)
+
+    def add_message(self, message):
+        enable(self.txtbox)
+        self.txtbox.insert(1.0, message + '\n')
+        disable(self.txtbox)
+
+
+class Name_window:
+    def __init__(self):
+        self.window = tk.Tk()
+
+        self.label = tk.Label(self.window, text='Your name:')
+        self.label.pack()
+
+        self.txtbox = tk.Entry(self.window)
+        self.txtbox.pack()
+
+        def go_button_click():
+            self.username = self.txtbox.get()
+            self.window.destroy()
+
+        self.button = tk.Button(self.window, text='GO', command=go_button_click)
+        self.button.pack()
+
+        self.username = None
+        self.window.mainloop()
+
+def choose_port():
+    with open('port.txt', 'r') as f:
+        s = f.readline()
+        if s == '0':
+            flag = 1
+            port = 17451
+        else:
+            flag = 0
+            port = 17452
+    
+    with open('port.txt', 'w') as f:
+        f.write(str(flag))
+
+    return port
+
+
+if __name__ == '__main__':
+    COM_LISTENER_PORT = choose_port()
+    COM_LISTENER_HOST = '127.0.0.1'
+    nwin = Name_window()
+    username = nwin.username
+    if username is None:
+        quit()
+    chwin = Chat_window(username)
---
+++ server.py
@@ -0,0 +1,132 @@
+'''
+Сервер для чата
+'''
+import socket
+from select import select
+from queue import Queue
+from queue import Empty
+from threading import Thread, Event
+HOST = '127.0.0.1'
+PORT = 28344
+
+def open_prepare_server_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(False)
+    sock.bind((HOST, PORT))
+    sock.listen(10)
+    return sock
+
+def access_event(s):
+    '''if need to access'''
+    return s is sock
+
+def close_event(data):
+    '''if need to close the connection'''
+    return not bool(data)
+
+def create_new_connection():
+    '''sock - main socket'''
+    conn, _ = sock.accept()
+    conn.setblocking(False)
+    inputs.append(conn)
+    client_socks.append(conn)
+    message_queues[conn] = Queue()
+    print('new connection was created')
+
+def close_connection(s):
+    '''s not is sock!'''
+    if s in outputs:
+        outputs.remove(s)
+    inputs.remove(s)
+    client_socks.remove(s)
+    s.close()
+    message_queues.pop(s, None)
+    print('connection was closed')
+
+def handle_input_message(s, data):
+    '''we have input message in data, let's handle it'''
+    global outputs
+    for key in client_socks:
+        if not key is s:
+            message_queues[key].put(data)
+    outputs = client_socks.copy()
+    print('message was recieved')
+
+def send_msg(s):
+    '''if there is message in queue, we send it'''
+    try:
+        next_msg = message_queues[s].get_nowait()
+    except Empty:
+        outputs.remove(s)
+    else:
+        s.send(next_msg)
+        print('message was sent')
+
+def handle_readable():
+    for s in readable:
+        if access_event(s):
+            create_new_connection()
+        else:
+            data = s.recv(4096)
+            if close_event(data):
+                close_connection(s)
+            else:
+                handle_input_message(s, data)
+
+def handle_writable():
+    for s in writable:
+        send_msg(s)
+
+def handle_exceptional():
+    for s in exceptional:
+        inputs.remove(s)
+        if s in outputs:
+            outputs.remove(s)
+        if not s is sock:
+            client_socks.remove(s)
+        message_queues.pop(s, None)
+        s.close()
+
+def close_all_sockets():
+    for s in client_socks:
+        s.close()
+    sock.close()
+
+def start_keyboard_input_thread():
+    def tarfun():
+        com = ''
+        while com != 'q':
+            com = input()
+
+        quit_event.set()
+        # (чтобы select перестал блокировать главный цикл в main)
+        quit_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        quit_sock.connect((HOST, PORT))
+        quit_sock.close()
+
+    keyboard_input_thread = Thread(target=tarfun)
+    keyboard_input_thread.start()
+        
+
+if __name__ == '__main__':
+    sock = open_prepare_server_socket()
+    inputs = [sock]
+    client_socks = []
+    outputs = []
+    message_queues = dict()
+    # нужно для того, чтобы прервать select(если захотим завершить сервер)
+    quit_event = Event()
+    start_keyboard_input_thread()
+    
+    while True:
+        print('iteration')
+        readable, writable, exceptional = select(inputs, outputs, inputs)
+        if quit_event.is_set():
+            break
+        handle_readable()
+        handle_writable()
+        handle_exceptional()
+
+    close_all_sockets()
+    print('all sockets were closed')
+    print('end of program')
